a4eae8771a2e77786180b6b05997072a6389bef6
rookiepeng <rookie.dev@gmail.com>
2017-03-08 11:35:05 -0600

    symmetrical array searching

--- symmetrical_linear_array_random_spacing.m
+++ symmetrical_linear_array_random_spacing.m
@@ -1,4 +1,4 @@
-%clear;
+clear;
 
 %% Configuration
 wavelength=1;
@@ -7,45 +7,38 @@ angleStep=0.05;
 theta=0:angleStep:360;
 elementNumber=16;
 spacingMIN=0.5;
-spacingMAX=5;
-mainbeam=7;
-thetaM=90;
+spacingMAX=2;
+mainbeam=10;
+thetaM=50;
 
 %% random search
-% d=[spacingMIN/2 + (spacingMAX/2-spacingMIN/2).*rand(1), spacingMIN + (spacingMAX-spacingMIN).*rand(1,elementNumber/2-1)];
-% %d=[spacingMIN/2,spacingMIN*ones(1,elementNumber/2-1)];
-% 
-% for nn=2:length(d)
-%     d(nn)=d(nn-1)+d(nn);
-% end
+d=[spacingMIN/2 + (spacingMAX/2-spacingMIN/2).*rand(1), spacingMIN + (spacingMAX-spacingMIN).*rand(1,elementNumber/2-1)];
+d1=fliplr(d);
+d=[0,d1(1:length(d1)-1),d1(length(d1))+d(1),d(2:length(d))];
+% d=[spacingMIN/2,spacingMIN*ones(1,elementNumber/2-1)];
+
+for nn=2:length(d)
+    d(nn)=d(nn-1)+d(nn);
+end
 
 %% check results
-load('resultd.mat');
-load('resultw.mat')
-[r,c]=size(resultd);
-nn=21;
-d=resultd(nn,:);
-w=resultw(nn,:)';
+% load('resultd.mat');
+% load('resultw.mat')
+% [r,c]=size(resultd);
+% nn=21;
+% d=resultd(nn,:);
+%w=resultw(nn,:)';
 
 %% Array factor
 A=zeros(length(theta),elementNumber);
-
 for nn=1:length(d)
-    A(:,nn)=exp(-1i*k*d(length(d)-nn+1)*cosd(theta));
-end
-
-for nn=1:length(d)
-    A(:,nn+length(d))=exp(1i*k*d(nn)*cosd(theta));
+    A(:,nn)=exp(1i*k*d(nn)*cosd(theta));
 end
 
 %% Main lobe
 A_M=zeros(1,elementNumber);
 for nn=1:length(d)
-    A_M(:,nn)=exp(-1i*k*d(length(d)-nn+1)*cosd(thetaM));
-end
-
-for nn=1:length(d)
-    A_M(:,nn+length(d))=exp(1i*k*d(nn)*cosd(thetaM));
+    A_M(:,nn)=exp(1i*k*d(nn)*cosd(thetaM));
 end
 
 %% Side lobe
@@ -54,11 +47,7 @@ theta_SL=[0:angleStep:thetaM-mainbeam/2,thetaM+mainbeam/2:angleStep:180];
 
 A_SL=zeros(length(theta_SL),elementNumber);
 for nn=1:length(d)
-    A_SL(:,nn)=exp(-1i*k*d(length(d)-nn+1)*cosd(theta_SL));
-end
-
-for nn=1:length(d)
-    A_SL(:,nn+length(d))=exp(1i*k*d(nn)*cosd(theta_SL));
+    A_SL(:,nn)=exp(1i*k*d(nn)*cosd(theta_SL));
 end
 
 %% Optimization
@@ -70,5 +59,6 @@ A_M*w==1;
 cvx_end
 
 %% Plot result
+%w=ones(1,elementNumber)';
 plot(theta,20*log10(abs(A*w))-max(20*log10(abs(A*w))));
 axis([0,180,-30,0]);
--- symmetrical_linear_array_random_spacing_search.m
+++ symmetrical_linear_array_random_spacing_search.m
@@ -6,65 +6,64 @@ angleStep=0.05;
 theta=0:angleStep:360;
 elementNumber=16;
 spacingMIN=0.5;
-spacingMAX=5;
-mainbeam=5;
-thetaM=45;
+spacingMAX=3;
+mainbeam1=8;
+% mainbeam2=20;
+thetaM1=90;
+% thetaM2=45;
 
 count=1;
 
 %%
-for m=1:50000
-    
+for m=1:10000
     d=[spacingMIN/2 + (spacingMAX/2-spacingMIN/2).*rand(1), spacingMIN + (spacingMAX-spacingMIN).*rand(1,elementNumber/2-1)];
+    d1=fliplr(d);
+    d=[0,d1(1:length(d1)-1),d1(length(d1))+d(1),d(2:length(d))];
     %d=[0.25,0.5*ones(1,elementNumber/2-1)];
-    
     for nn=2:length(d)
         d(nn)=d(nn-1)+d(nn);
     end
     %theta=90;
+    
     A=zeros(length(theta),elementNumber);
     for nn=1:length(d)
-        A(:,nn)=exp(-1i*k*d(length(d)-nn+1)*cosd(theta));
+        A(:,nn)=exp(1i*k*d(nn)*cosd(theta));
     end
     
+    A_M=zeros(1,elementNumber);
     for nn=1:length(d)
-        A(:,nn+length(d))=exp(1i*k*d(nn)*cosd(theta));
+        A_M(:,nn)=exp(1i*k*d(nn)*cosd(thetaM1));
     end
-    
-    for nn=1:length(d)
-    AM(:,nn)=exp(-1i*k*d(length(d)-nn+1)*cosd(thetaM));
-end
-
-for nn=1:length(d)
-    AM(:,nn+length(d))=exp(1i*k*d(nn)*cosd(thetaM));
-end
-    
     %w=ones(16,1);
     
-    theta_SL=[0:angleStep:thetaM-mainbeam/2,thetaM+mainbeam/2:angleStep:180];
-    theta_ML=90+mainbeam/2:angleStep:180;
+    theta_SL1=[0:angleStep:thetaM1-mainbeam1/2,thetaM1+mainbeam1/2:angleStep:180];
+%     theta_SL2=[0:angleStep:thetaM2-mainbeam2/2,thetaM2+mainbeam2/2:angleStep:180];
+    %theta_ML=90+mainbeam/2:angleStep:180;
     
-    A_SL=zeros(length(theta_SL),elementNumber);
+    A_SL1=zeros(length(theta_SL1),elementNumber);
     for nn=1:length(d)
-        A_SL(:,nn)=exp(-1i*k*d(length(d)-nn+1)*cosd(theta_SL));
+        A_SL1(:,nn)=exp(1i*k*d(nn)*cosd(theta_SL1));
     end
     
-    for nn=1:length(d)
-        A_SL(:,nn+length(d))=exp(1i*k*d(nn)*cosd(theta_SL));
-    end
-    
-    n=elementNumber;
+%     A_SL2=zeros(length(theta_SL2),elementNumber);
+%     for nn=1:length(d)
+%         A_SL2(:,nn)=exp(1i*k*d(nn)*cosd(theta_SL2));
+%     end
     
     cvx_begin
-    variable w(n) complex
-    minimize( max(abs(A_SL*w)) )
+    variable w(elementNumber) complex
+    minimize( max(abs(A_SL1*w)) )
     subject to
-    ones(1,elementNumber)*w==1;
+    A_M*w==1;
     cvx_end
     
-    if max(abs(A_SL*w))<10^(-10/20)
+    if max(abs(A_SL1*w))<10^(-10/20)
         resultd(count,:)=d;
         resultw(count,:)=w';
+        resultSLL(count)=max(abs(A_SL1*w));
+        save('resultd.mat', 'resultd');
+        save('resultw.mat', 'resultw');
+        save('SLL.mat', 'resultSLL');
         count=count+1;
         %plot(theta,20*log10(abs(A*x(2:9))));
         %axis([0,180,-70,0]);
@@ -72,5 +71,5 @@ end
     end
 end
 
-%plot(theta,20*log10(abs(A*w))-max(20*log10(abs(A*w))));
-%axis([0,180,-30,0]);
+% plot(theta,20*log10(abs(A*w))-max(20*log10(abs(A*w))));
+% axis([0,180,-30,0]);
